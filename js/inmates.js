0e55b701324f8425a6761e923aec92c305f7f422
samah <samahmajadla@gmail.com>
2020-11-26 16:22:34 -0500

    only view that exists is of packages, with bullet lists of their contents. edit and delete package available for each

--- js/inmates.js
+++ js/inmates.js
@@ -46,17 +46,6 @@ inmateFunctions = function(){
     })
 }
 
-
-async function getPackages(inmate_id){
-    let data = await fetch(`http://localhost:8080/getPackages?id=${inmate_id}`, {
-        method: 'get'
-    }).then((response)=>response.json())
-    .then(data => {
-        return data
-    })
-    return data
-}
-
     function displayError(){
         const resultsContainer = getAndClearInmateResultsElement();
         const errorMessage = "Input not understood. Please check ID#."
@@ -70,12 +59,11 @@ async function getPackages(inmate_id){
         displayNameAndId(inmateInfo, resultsContainer);
 
         displayAddPackageButton(resultsContainer);
-        if (inmateInfo.packages.length > 0){
-        displayEditOrPrintInvoiceButton(resultsContainer, inmateInfo.id)
+        if(inmateInfo.packages.length > 0  ){
+            createEditOrPrintPackageTable(resultsContainer, inmateInfo.packages);
         }
-        const tableElement = createPackageTableElementAndHeader(resultsContainer)
-        displayPackagesByBook(inmateInfo.packages, tableElement);
     }
+
     function displaySearchButton(container){
         const div = document.createElement("div")
         div.style.textAlign = "left"
@@ -104,49 +92,10 @@ async function getPackages(inmate_id){
         }
     }
 
-    function displayEditOrPrintInvoiceButton(container, id){
-        const editOrPrintPackage = helperFunctions.createButton("Edit or print old package")
-        editOrPrintPackage.id = editOrPrintButtonId
-        editOrPrintPackage.style.background = "LightCoral"
-        let buttonToDelete = document.getElementById(seeAllBooksButtonId)
-        if (buttonToDelete != null){
-             buttonToDelete.parentNode.replaceChild(editOrPrintPackage, buttonToDelete)
-        } else {
-            container.appendChild(editOrPrintPackage)
-        }
-        editOrPrintPackage.onclick = () => {
-            let packages = getPackages(id)
-            packages.then((data)=>{
-                            createEditOrPrintPackageTable(container, data)
-
-            })
-            displaySeeAllBooksButton(container, id)  
-        }
-    }
-
-    function displaySeeAllBooksButton(container, id){
-        const seeAllBooks = helperFunctions.createButton("See all books/zines received")
-        seeAllBooks.id = seeAllBooksButtonId
-
-        let buttonToDelete = document.getElementById(editOrPrintButtonId)
-        if (buttonToDelete != null){
-             buttonToDelete.parentNode.replaceChild(seeAllBooks, buttonToDelete)
-        }
-        seeAllBooks.onclick = () => {
-            const tableElement = createPackageTableElementAndHeader(container)
-            let packages = getPackages(id)
-            packages.then((data) => {
-                displayPackagesByBook(data, tableElement);
-            })
-            displayEditOrPrintInvoiceButton(container, id)
-        }
-    }
-
     function createEditOrPrintPackageTable(container, packages){
-
-        let tableElement = document.getElementById(packageTableId)
-        tableElement.innerHTML = ""
-
+        
+        let tableElement = getAndClearPackageTableElement()
+        
         const headerRow = document.createElement('tr')
 
         const packageContents = document.createElement('th')
@@ -164,7 +113,7 @@ async function getPackages(inmate_id){
         let count = 0;
 
         packages.slice().reverse().forEach(package => {
-            let packageElement = generatePackageContentText(package);
+            let packageElement = generatePackageContentElement(package);
 
             const packageRow = document.createElement('tr')
             if (count%2 == 0){
@@ -172,7 +121,8 @@ async function getPackages(inmate_id){
             }
             count++
             const packageCell = document.createElement('td')
-          
+            packageCell.style.paddingTop = "10px"
+            packageCell.style.paddingBottom = "10px"
 
             packageCell.appendChild(packageElement)
             packageRow.appendChild(packageCell)
@@ -200,7 +150,7 @@ async function getPackages(inmate_id){
 
       
     }
-  function generatePackageContentText(package) {
+  function generatePackageContentElement(package) {
             let div = document.createElement("div")
             div.style.textAlign = "left"
             div.style.paddingLeft = "20px"
@@ -229,114 +179,16 @@ async function getPackages(inmate_id){
             return div
         }
 
-    function createPackageTableElementAndHeader(resultsContainer){
+    function getAndClearPackageTableElement(){
         let tableElement = document.getElementById(packageTableId)
         if (tableElement == null){
             tableElement = document.createElement('table');
             tableElement.id = packageTableId
-        } else {
-            tableElement.innerHTML = ""
-        }
-       
-        const headerRow = document.createElement('tr');
-
-        const date = document.createElement('th');
-        const title = document.createElement('th');
-        const author = document.createElement('th');
-
-        date.textContent = 'Date sent'
-        title.textContent = 'Title'
-        author.textContent = 'Author'
-
-        headerRow.appendChild(date)
-        headerRow.appendChild(title)
-        headerRow.appendChild(author)
-
-        tableElement.appendChild(headerRow)
-
-        resultsContainer.appendChild(tableElement)
+        } 
+        tableElement.innerHTML = ""
         return tableElement
     }
 
-    function displayPackagesByBook(packages, tableElement){
-        let count = 0
-        packages.slice().reverse().forEach(package => {
-            package.books.forEach(book => {
-            
-            const bookRow = document.createElement('tr');
-            if (count %2 == 0){
-                bookRow.style.background = "Gainsboro"
-            }
-            count++
-
-            const date = document.createElement('td');
-            const title = document.createElement('td');
-            const author = document.createElement('td');
-
-            date.textContent = package.date
-            title.textContent = book.title
-            author.textContent = book.authors[0]
-
-            title.style.fontStyle = 'italic'
-
-            bookRow.appendChild(date)
-            bookRow.appendChild(title)
-            bookRow.appendChild(author)
-            tableElement.appendChild(bookRow)
-        })
-
-        package.zines.forEach(zine => {
-            
-            const zineRow = document.createElement('tr');
-            if(count%2 == 0){
-                zineRow.style.background = "Gainsboro"
-            }
-            count++
-            const date = document.createElement('td');
-            const title = document.createElement('td');
-            const author = document.createElement('td');
-
-            date.textContent = package.date
-            title.innerHTML = `<b>${zine.threeLetterCode}</b>  - ${zine.title}`
-            author.textContent = "ZINE"
-
-            title.style.fontStyle = 'italic'
-
-            zineRow.appendChild(date)
-            zineRow.appendChild(title)
-            zineRow.appendChild(author)
-            tableElement.appendChild(zineRow)
-        })
-
-        package.resources.forEach(resource => {
-            
-                const resourceRow = document.createElement('tr');
-                if (count%2 == 0){
-                    resourceRow.style.background= "Gainsboro"
-                }
-                const date = document.createElement('td');
-                const title = document.createElement('td');
-                const author = document.createElement('td');
-    
-                date.textContent = package.date
-                title.innerHTML = `${resource.title}`
-                author.textContent = `${resource.authors[0]}`
-    
-                title.style.fontStyle = 'italic'
-    
-                resourceRow.appendChild(date)
-                resourceRow.appendChild(title)
-                resourceRow.appendChild(author)
-                tableElement.appendChild(resourceRow)
-            }
-
-        )
-
-
-
-        });
-
-    }
 
     function displayNameAndId(inmateInfo, resultsContainer) {
         const div = document.createElement("div")
