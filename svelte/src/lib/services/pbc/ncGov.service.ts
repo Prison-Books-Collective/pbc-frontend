93d2797b5830d512b457ee149083a85f3e4ee2db
Samah Majadla <smajadla@smajadlaY16RJ.vmware.com>
2023-01-20 16:44:23 -0500

    can enter author or group in nonisbn book

--- svelte/src/lib/services/pbc/ncGov.service.ts
+++ svelte/src/lib/services/pbc/ncGov.service.ts
@@ -2,18 +2,17 @@ import { delay } from "$util/time"
 
 
 export const fetchRecipientLocation = async (assignedId) => {
-   window.open('https://webapps.doc.state.nc.us/opi/offendersearch.do?method=view')
-      // let displayTable = govWindow.document.getElementsByClassName("displaytable")
-      // console.log("got table")
-      // console.log(displayTable)
-   
-      // let idField = displayTable[0].children[0].children[3].children[1].children[0] as HTMLElement
-      // idField.value = assignedId
-      // let submitButton = displayTable[0].children[0].children[9].children[0].children[0].children[0].children[0].children[0].children[0] as HTMLElement
-      // submitButton.click()
-   
-   }
+   // let inmateSearch = await window.open('https://www.ncinmatesearch.org/NC_DPS.html')
       
+    
+   //    inmateSearch.onload = () => {
+   //       let searchField = inmateSearch.document.getElementsByName("searchOffenderId")[0] as HTMLInputElement
+   //       searchField.value = "hi"
+   //       console.log(searchField)
+   //    }
+    
+   }
+    
          
     
    
